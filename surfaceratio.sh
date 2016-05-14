7f5487b1ac718255275564634fcaef3a305f005d
aosoianu <anastasia.osoianu@gmail.com>
2016-05-14 14:50:30 +0200

    stl baboon

---
+++ surfaceratio.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+# original
+db1=( baboon capuchin ce_macaque douroucouli mangabey galago slow_loris orangutan )
+
+src=/Users/ghfc/Documents/Dropbox/hugo_is_dead/scripts/BrainCatalogueWorkflow/meshes_centered
+
+sr=/Users/ghfc/Documents/Dropbox/hugo_is_dead/scripts/surfaceratio/surfaceratio
+mg=/Users/ghfc/Applications/brainbits/meshgeometry/meshgeometry_mac
+
+# process
+for i in ${db1[@]}
+do
+	mkdir $dst/$i
+	mkdir $src/$i/surfaceratio
+
+    $mg -i $src/$i/left.ply -o $src/$i/surfaceratio/left.pial
+    $mg -i $src/$i/right.ply -o $src/$i/surfaceratio/right.pial
+
+    $sr $src/$i/surfaceratio/left.pial $src/$i/surfaceratio/left.curv
+    $sr $src/$i/surfaceratio/right.pial $src/$i/surfaceratio/right.curv
+done
